a3f0b3b9e438b6b08081f23f1264ff1a66aebe7a
Gillyweed <43531452+Gillyweed@users.noreply.github.com>
2025-03-14 16:54:01 -0300

    updated main.py to read frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as file:
+        file_contents = file.read()
+        return file_contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
